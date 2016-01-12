48a0b620e0127c4c5e2237474ccfac47709deb21
rehamalisalehhosni <engreham.ali2011@gmail.com>
2016-01-12 09:17:00 +0200

    last

--- js/javascript.js
+++ js/javascript.js
@@ -252,36 +252,3 @@ function MoveAlien(dir, cur) {
     }
 }
 
-// Fathi
-function alienOperations(id) {
-    var click = Game.levels.Aliens[id - 1].noClick;
-    click++;
-
-    var shape = Game.levels.Aliens[id - 1].id;
-    var x = "alian_" + shape;
-    var a = document.getElementById(x);
-    if (click == Game.levels.diedAfter) {
-        a.setAttribute('style', 'display:none');
-        var audio = new Audio('./audio/neck_snap.mp3');
-        audio.play();
-        Game.levels.Aliens[id - 1].live = false;
-        //  incrementScore();
-        tempalinenarray.push(id - 1);
-        checkAlines();
-    }
-}
-function checkAlines() {
-    var aliens = document.getElementById('aliens');
-    if (tempalinenarray.length == Game.levels.Aliens.length) {
-        console.log('new');
-        Game.levels.noAliens = Game.levels.noAliens + 10;
-        Game.levels.Nolevel = Game.levels.Nolevel++;
-        Game.levels.timemovein3px = 60;
-        aliens.innerHTML = '';
-        Game.levels.Aliens=[];
-        tempalinenarray=[];
-        StartGameContainer(Game.username, Game.levels.Nolevel, Game.levels.noAliens, Game.time, Game.charName, Game.levels.diedAfter, 0, Game.lives, Game.levels.timemovein3px);
-
-    }// enf if
-
-} //end of fun
